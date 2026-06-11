2725975d898f8c16d3114a32cb810c9cb5409686
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2026-06-11 09:20:22 +0200

    Subir ejercicio03

--- thymeleaf/src/main/java/com/optativa/thymeleaf/controlador/ProductoControlador.java
+++ thymeleaf/src/main/java/com/optativa/thymeleaf/controlador/ProductoControlador.java
@@ -17,9 +17,9 @@ public class ProductoControlador {
     @GetMapping("/productos")
     public String listarProductos(Model modelo) {
         List<Producto> listaProductos = new ArrayList<>();
-        listaProductos.add(new Producto("Teclado", 25.99, "Informática"));
-        listaProductos.add(new Producto("Silla de oficina", 89.50, "Mobiliario"));
-        listaProductos.add(new Producto("Cuaderno", 1.75, "Papelería"));
+        // listaProductos.add(new Producto("Teclado", 25.99, "Informática"));
+        // listaProductos.add(new Producto("Silla de oficina", 89.50, "Mobiliario"));
+        // listaProductos.add(new Producto("Cuaderno", 1.75, "Papelería"));
 
         modelo.addAttribute("listaProductos", listaProductos);
         return "productos";
